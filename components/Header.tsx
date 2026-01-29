18ffab7f4f449e50aae3af434b50a923b23596ee
unknown <ron.garcia.santos@gmail.com>
2026-01-29 22:10:34 +0800

    update

---
+++ components/Header.tsx
@@ -0,0 +1,15 @@
+export default function Header ()
+{
+    return (
+        <header 
+            className="flex border"
+        >
+            <nav
+                className="flex justify-between p-7 w-screen text-lg "
+            >
+                <span>BMS</span>
+                <span>Profile</span>
+            </nav>
+        </header>
+    )
+}
---
+++ components/SideNav.tsx
@@ -0,0 +1,10 @@
+export default function SideNav ()
+{
+    return (
+        <nav
+            className="flex w-[280px] h-screen border"
+        >
+            
+        </nav>
+    )
+}

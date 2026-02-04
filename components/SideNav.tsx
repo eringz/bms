cea624a4f8c96949f530756b752589f17065872e
unknown <ron.garcia.santos@gmail.com>
2026-02-04 12:37:41 +0800

    update

--- components/SideNav.tsx
+++ components/SideNav.tsx
@@ -2,7 +2,7 @@ export default function SideNav ()
 {
     return (
         <nav
-            className="flex w-[280px] h-screen border bg-linear-to-b from-[#002F61]/98 via-[#002F61]/80 to-[#002F61]/99 overflow-hidden border-r-white/20 border-t-none"
+            className="flex w-[60px] h-screen border-r-2 border-cyan-500 shadow-[4px_0_10px_rgba(0,0,0,0.15)] shadow-cyan-500/50  bg-linear-to-b from-[#002F61]/98 via-[#002F61]/80 to-[#002F61]/99 overflow-hidden border-r-white/20 border-t-none"
         >
            
         </nav>
